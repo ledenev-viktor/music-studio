27782babf18bfaedfc9135447d290b36ee278cb1
YuliaKletsova <july_2000@mail.ru>
2024-04-30 21:54:52 +0400

    test

--- src/app/admin/component-app.tsx
+++ src/app/admin/component-app.tsx
@@ -1,7 +1,18 @@
 "use client";
-import { Admin } from "react-admin";
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
-const AdminApp = () => <Admin />;
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const AdminApp = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users"
+      list={ListGuesser}
+      edit={EditGuesser}
+      recordRepresentation="name"
+    />
+  </Admin>
+);
 
 export default AdminApp;
