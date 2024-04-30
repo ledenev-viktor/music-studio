3ecaefd07794cda67cf89f92d6406e24c65aa6e7
YuliaKletsova <july_2000@mail.ru>
2024-05-01 03:06:31 +0400

    remove unused export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
-export const roboto = Roboto({
+const roboto = Roboto({
     subsets: ['latin'],
     weight: ['100', '300', '400', '500', '700', '900'],
     variable: '--font-roboto',
