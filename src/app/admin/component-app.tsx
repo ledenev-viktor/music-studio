3ea28ba0e3b4d1f0ea46d2134e4812e49cbc871d
YuliaKletsova <july_2000@mail.ru>
2024-04-30 22:29:37 +0400

    layout-test

--- src/app/admin/component-app.tsx
+++ src/app/admin/component-app.tsx
@@ -1,11 +1,31 @@
 "use client";
-import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  Layout,
+  AppBar,
+  TitlePortal,
+} from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import React from "react";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
+const MyAppBar = () => (
+  <AppBar sx={{
+        color: '#2C40EA',
+        '& .RaAppBar-toolbar': { padding: 0 },
+    }}>
+    <div>layout</div>
+  </AppBar>
+);
+
+const MyLayout = (props: any) => <Layout {...props} appBar={MyAppBar} />;
+
 const AdminApp = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} layout={MyLayout}>
     <Resource
       name="users"
       list={ListGuesser}
