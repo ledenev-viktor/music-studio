e652425dddd1a4d08db29d5c076c5a3f47a440f4
ledenev-viktor <ledenev4252@gmail.com>
2024-05-06 01:08:29 +0300

    created footer, header - intermediate commit

---
+++ src/components/wigets/footer/index.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+const FooterBase = () => {
+    return <div>Footer</div>;
+};
+
+export const Footer = styled(FooterBase)``;
---
+++ src/components/wigets/header/index.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+const HeaderBase = () => {
+    return <div>Header</div>;
+};
+
+export const Header = styled(HeaderBase)``;
