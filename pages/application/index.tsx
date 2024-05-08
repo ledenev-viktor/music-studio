a88961960f6f2bfc2a902993ad60823d2258704e
ledenev-viktor <ledenev4252@gmail.com>
2024-05-08 23:39:16 +0300

    finalizing the form, sending data

--- pages/application/index.tsx
+++ pages/application/index.tsx
@@ -1,18 +1,5 @@
-import { Flex, Typography } from 'antd';
+import { RegistrationForm } from '~components/wigets/registration';
 
 export default function Page() {
-    const { Title } = Typography;
-
-    return (
-        <Flex
-            vertical
-            justify="center"
-            align="center"
-            style={{ padding: '30px' }}
-        >
-            <Flex justify="start" style={{ width: '100%' }}>
-                <Title>Application</Title>
-            </Flex>
-        </Flex>
-    );
+    return <RegistrationForm />;
 }
--- src/components/wigets/registration/types/form.ts
+++
@@ -1,5 +0,0 @@
-export type TimeSlot = {
-    id: string;
-    value: string;
-    label: string;
-};
