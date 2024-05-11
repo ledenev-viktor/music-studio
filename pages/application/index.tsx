1c5eb0094646f48959d142d5c65b0d4d0b085dc7
ledenev <ledenev4252@gmail.com>
2024-05-11 14:59:21 +0300

    create registration form

--- pages/application/index.tsx
+++ pages/application/index.tsx
@@ -1,5 +1,14 @@
-import { RegistrationForm } from '~components/wigets/registration';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { RegistrationForm } from '~components/widgets/registration';
 
 export default function Page() {
     return <RegistrationForm />;
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['common'])),
+        },
+    };
 }
