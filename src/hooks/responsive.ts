67ff351a7b0d325db19d3bf6e9a4595fadfff465
ledenev <ledenev4252@gmail.com>
2024-05-11 14:53:29 +0300

    added hook responsive

--- src/hooks/responsive.ts
+++ src/hooks/responsive.ts
@@ -1,3 +1,5 @@
 import { useMediaQuery } from 'react-responsive';
+import { BREAKPOINTS } from '~constants/breakpoints';
 
-export const useMobile = () => useMediaQuery({ query: '(max-width: 767px)' });
+export const useMobile = () =>
+    useMediaQuery({ query: `(max-width: ${BREAKPOINTS.mobile})` });
