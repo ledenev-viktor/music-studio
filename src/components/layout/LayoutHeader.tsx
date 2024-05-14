a62314cf1d4e87365d9b8c4650cc8b1acc0aa710
YuliaKletsova <july_2000@mail.ru>
2024-05-14 05:40:44 +0400

    menu try 2

--- src/components/layout/LayoutHeader.tsx
+++ src/components/layout/LayoutHeader.tsx
@@ -1,17 +1,62 @@
 'use client';
 import React from 'react';
-import { ConfigProvider, Flex, Layout } from 'antd';
+import { ConfigProvider, Flex, Layout, Menu } from 'antd';
+import { useTranslation } from 'react-i18next';
+import Link from 'next/link';
+import styled from '@emotion/styled';
 import { COLORS } from '~variables';
 import { LangSwitch, Logo } from '~components/ui/home';
-import { Menu } from './Menu/Menu';
+
+const MenuLabel = styled(Link)`
+    font-size: 16px;
+`;
 
 export const LayoutHeader = () => {
+    const { t } = useTranslation();
+
+    const menuItems = [
+        {
+            label: (
+                <MenuLabel href="/home" passHref>
+                    {t('header_menu_home')}
+                </MenuLabel>
+            ),
+            key: 1,
+        },
+        {
+            label: (
+                <MenuLabel href="/application" passHref>
+                    {t('header_menu_application')}
+                </MenuLabel>
+            ),
+            key: 2,
+        },
+        {
+            label: (
+                <MenuLabel href="/contacts" passHref>
+                    {t('header_menu_contacts')}
+                </MenuLabel>
+            ),
+            key: 3,
+        },
+    ];
+
     return (
         <ConfigProvider
             theme={{
                 components: {
                     Layout: {
                         headerBg: `${COLORS.blue}`,
+                    },
+                    Menu: {
+                        itemBg: 'none',
+                        itemColor: `${COLORS.white}`,
+                        itemHoverColor: `${COLORS.white}`,
+                        horizontalItemHoverBg: `${COLORS.blueHovered}`,
+                        horizontalItemSelectedBg: `${COLORS.blueHovered}`,
+                        horizontalItemSelectedColor: `${COLORS.white}`,
+                        activeBarHeight: 0,
+                        popupBg: COLORS.blue,
                     },
                 },
             }}
@@ -32,7 +77,18 @@ export const LayoutHeader = () => {
                         margin: '0 auto',
                     }}
                 >
+                    <Menu
+                        mode="horizontal"
+                        style={{
+                            minWidth: 0,
+                            flex: '1',
+                            maxWidth: '450px',
+                            width: '100%',
+                        }}
+                        items={menuItems}
+                    />
                     <Flex gap={20} align="center">
+                        <LangSwitch />
                         <Logo
                             link="/"
                             src={'/logo.png'}
@@ -41,9 +97,7 @@ export const LayoutHeader = () => {
                             height="50"
                             style={{ marginRight: '5px' }}
                         />
-                        <LangSwitch />
                     </Flex>
-                    <Menu />
                 </Flex>
             </Layout.Header>
         </ConfigProvider>
