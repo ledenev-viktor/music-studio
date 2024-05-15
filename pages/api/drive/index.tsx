989b35e91221ca0b6e0b15bb123f4f72fa435f6a
YuliaKletsova <july_2000@mail.ru>
2024-05-15 07:36:11 +0400

    api-fix

--- pages/api/drive/index.tsx
+++ pages/api/drive/index.tsx
@@ -1,5 +1,13 @@
 import { google } from 'googleapis';
-import jwtClient from '../jwt';
+import googleConfig from '~lib/google';
+
+const jwtClient = new google.auth.JWT(
+    googleConfig.clientEmail,
+    undefined,
+    googleConfig.privateKey,
+    'https://www.googleapis.com/auth/docs https://www.googleapis.com/auth/drive',
+    undefined,
+);
 
 export const drive = google.drive({
     version: 'v3',
