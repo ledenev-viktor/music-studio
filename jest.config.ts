fce87f768234dc3ce73782cf5b0441551e4a97af
ledenev-viktor <ledenev4252@gmail.com>
2024-05-19 17:30:21 +0300

    configuration lint, tsconfig for jest, added libs dayjs

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.spec.json',
+        },
+    },
     testEnvironment: 'jest-environment-jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
