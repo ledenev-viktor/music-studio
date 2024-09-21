12c9c9936f443a38a9444ab5e9c422d4cba4c6f8
ledenev-viktor <ledenev4252@gmail.com>
2024-09-21 21:39:06 +0300

    excluded booking past hours

--- src/utils/getWorkingHours.ts
+++ src/utils/getWorkingHours.ts
@@ -1,9 +1,28 @@
-export const getWorkingHours = (startHour: number, endHour: number) => {
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const getWorkingHours = (
+    startHour: number,
+    endHour: number,
+    day: string,
+) => {
     if (endHour <= startHour) return [];
+
+    const dateNow = '2024-09-22T11:00:00';
+
     const result = [];
 
     for (let hour = startHour; hour < endHour; hour++) {
-        result.push([String(hour), String(hour + 1)]);
+        const dateStart = day + `T${hour}:00:00`;
+
+        const dateWithSubtractedHour = dayjs(dateStart).subtract(1, 'hour');
+
+        if (!dayjs(dateNow).isAfter(dateWithSubtractedHour)) {
+            result.push([String(hour), String(hour + 1)]);
+        }
     }
 
     return result;
