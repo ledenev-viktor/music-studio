9841386cd5261d9cd907bb0f48e8d15d523f6125
ledenev-viktor <ledenev4252@gmail.com>
2025-01-03 20:47:23 +0300

    fix pacakges, next.config

--- next.config.js
+++ next.config.js
@@ -19,6 +19,9 @@ module.exports = {
     compiler: {
         styledComponents: true,
     },
+    experimental: {
+        esmExternals: true,
+    },
     i18n,
     images: {
         remotePatterns: [
