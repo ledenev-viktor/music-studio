a9b523696063096bb71e0bbb29a0eb96811ba0e0
ledenev-viktor <ledenev4252@gmail.com>
2025-02-17 23:15:15 +0300

    fix next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = {
     reactStrictMode: true,
+    experimental: {
+        esmExternals: true,
+    },
     serverRuntimeConfig: {
         privateKey: process.env.PRIVATE_KEY,
         clientEmail: process.env.CLIENT_EMAIL,
